b3839f22bd646edd07b5ee687a8d1d02c0118e7b
IllusiveBagel <hawkentroop@gmail.com>
2020-09-24 23:40:49 +0100

    Changed File Structure

---
+++ Learning/Lesson 03/boot_sect_memory.asm
@@ -0,0 +1,17 @@
+[org 0x7c00]
+mov ah, 0x0e
+
+; Having solved the memory offset problem with 'org', this will now print the secret
+mov al, [the_secret]
+int 0x10
+
+jmp $ ; infinite loop
+
+the_secret:
+    ; ASCII code 0x58 ('X') is stored just before the zero-padding.
+    ; On this code that is at byte 0x2d (check it out using 'xxd file.bin')
+    db "X"
+
+; zero padding and magic bios number
+times 510-($-$$) db 0
+dw 0xaa55
