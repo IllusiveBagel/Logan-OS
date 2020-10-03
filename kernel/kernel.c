269ec95d5fc279c1cd2d79c3b642f03480c72537
IllusiveBagel <hawkentroop@gmail.com>
2020-10-03 13:14:01 +0100

    Added Interupts, timer and keyboard

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,7 +1,15 @@
 #include "../drivers/screen.h"
 #include "util.h"
+#include "../cpu/isr.h"
+#include "../cpu/timer.h"
+#include "../drivers/keyboard.h"
 
 void main() {
+    isr_install();
+
+    asm volatile("sti");
+    init_timer(50);
+
     clear_screen();
     kprint("dP                                                      .88888.  .d88888b\n");
     kprint("88                                                     d8\'   `8b 88.    \"'\n");
@@ -11,4 +19,6 @@ void main() {
     kprint("88888888P `88888P' `8888P88 `88888P8 dP    dP           `8888P'   Y88888P  \n");
     kprint("oooooooooooooooooooo~~~~.88~ooooooooooooooooooooooooooooooooooooooooooooooo\n");
     kprint("                    d8888P\n");
+
+    init_keyboard();
 } 
